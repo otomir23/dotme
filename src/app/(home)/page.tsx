e641bd3791e90f4fbc2c739f4be307b8efe9a26a
Damir Modyarov <otomir@yandex.ru>
2023-01-27 19:07:28 +0300

    Create "About Me" section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,11 +2,13 @@ import ThemeToggle from "@/app/theme-toggle";
 import {
     Bars3Icon,
     BriefcaseIcon,
-    LinkIcon,
+    LinkIcon, MapPinIcon,
     PencilIcon,
     WrenchScrewdriverIcon,
     XMarkIcon
 } from "@heroicons/react/20/solid";
+import Image from "next/image";
+import avatar from "@/../public/avatar.jpg";
 
 export default function Home() {
     const nav = [
@@ -18,7 +20,7 @@ export default function Home() {
 
     return (
         <>
-            <aside className="flex items-center justify-between mb-8">
+            <aside className="flex items-center justify-between mb-8 sm:mb-16 sm:mt-8">
                 <label>
                     <input type="checkbox" className="hidden peer" />
                     <Bars3Icon className="w-6 h-6 sm:hidden cursor-pointer" />
@@ -46,7 +48,29 @@ export default function Home() {
                 </label>
                 <ThemeToggle />
             </aside>
-            <p className="text-2xl font-bold">Hello, world!</p>
+            <section id="me">
+                <div className="flex flex-row gap-8 items-center">
+                    <Image src={avatar} alt="My avatar"
+                           width={128} height={128} quality={100}
+                           className="rounded-full aspect-square object-cover w-32 h-32"
+                    />
+                    <div className="flex flex-col gap-2">
+                        <h1 className="text-2xl sm:text-4xl font-bold text-neutral-900 dark:text-neutral-100">
+                            Damir Modyarov
+                        </h1>
+                        <p className="text-neutral-700 dark:text-neutral-300 text-sm sm:text-lg">
+                            Web Developer, UI Designer
+                        </p>
+                    </div>
+                </div>
+                <p className="text-neutral-700 dark:text-neutral-300 text-sm sm:text-lg mt-4 sm:mt-8">
+                    Hi! I’m Damir Modyarov <span className="text-neutral-500"> (@otomir23)</span>.
+                    I live in <MapPinIcon className="w-4 h-4 inline text-blue-600" />{" "}
+                    <span className="underline text-blue-600 cursor-help" title="Great city!">Moscow, Russia</span> and
+                    I am a developer, designer and a very lazy person. I primary specialize in web design and development,
+                    but I also sometimes create Minecraft mods or Discord bots.
+                </p>
+            </section>
         </>
     )
 }
