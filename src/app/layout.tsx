3c9d59e4b3f9285f0e8d066632b75ed8f6c5cc3a
Damir Modyarov <otomir@yandex.ru>
2023-01-27 17:16:28 +0300

    Implement dark theme using next-themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 
 import {Inter} from '@next/font/google'
 import React from "react";
+import Providers from "@/app/providers";
 
 const inter = Inter({subsets: ['latin']})
 
@@ -10,7 +11,9 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
         <html lang="en">
             <head />
             <body className={inter.className}>
-                {children}
+                <Providers>
+                    {children}
+                </Providers>
             </body>
         </html>
     )
