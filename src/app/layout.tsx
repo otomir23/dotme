a94525ad71d13c220a7b25f2d8e8b5464797c1e7
Damir Modyarov <otomir@yandex.ru>
2023-01-27 17:11:25 +0300

    Move home page to its own group

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,10 @@ const inter = Inter({subsets: ['latin']})
 export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
         <html lang="en">
-        <head/>
-        <body className={inter.className}>{children}</body>
+            <head />
+            <body className={inter.className}>
+                {children}
+            </body>
         </html>
     )
 }
