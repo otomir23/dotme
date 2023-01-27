74bcdaa8b9327f387904edc09fd9283e773113d9
Damir Modyarov <otomir@yandex.ru>
2023-01-27 16:30:04 +0300

    Prepare dev environment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,15 @@
 import './globals.css'
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+import {Inter} from '@next/font/google'
+import React from "react";
+
+const inter = Inter({subsets: ['latin']})
+
+export default function RootLayout({children}: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+        <head/>
+        <body className={inter.className}>{children}</body>
+        </html>
+    )
 }
