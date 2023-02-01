2d8fac754216cebaf6e80b73530d8504eb042633
Damir Modyarov <otomir@yandex.ru>
2023-02-01 11:18:14 +0300

    Update image sources

--- next.config.js
+++ next.config.js
@@ -4,7 +4,17 @@ const nextConfig = {
     appDir: true,
   },
   images: {
-    domains: ['images.unsplash.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.amazonaws.com',
+        pathname: '/secure.notion-static.com/**',
+      },
+    ],
   }
 }
 
