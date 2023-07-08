f6776edf664beb6a6b8e10ca85e4eaedd556e90a
Damir Modyarov <otomir@yandex.ru>
2023-07-09 00:46:07 +0300

    Add placeholder for project search

--- src/app/project-search.tsx
+++ src/app/project-search.tsx
@@ -19,7 +19,9 @@ export default function ProjectSearch() {
                     router.replace(`?q=${e.target.value}${searchParams.has('t') ? `&t=${searchParams.get('t')}` : ''}`)
                 }}
                 className="border border-neutral-200 dark:border-neutral-800 bg-neutral-50 dark:bg-neutral-900 text-lg
-                py-2 px-4 rounded-lg appearance-none w-full focus:outline-none focus:ring ring-neutral-100 dark:ring-neutral-900"
+                py-2 px-4 rounded-lg appearance-none w-full focus:outline-none focus:ring ring-neutral-100 dark:ring-neutral-900
+                placeholder:text-neutral-400 dark:placeholder:text-neutral-600"
+                placeholder="Find projects..."
             />
             {transition && <div className="animate-spin border border-transparent border-b-neutral-500 aspect-square h-4 rounded-full" />}
         </>
