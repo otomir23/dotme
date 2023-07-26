6f2644363e63ea450c3f00c94cb985cfe541ffad
Damir Modyarov <otomir@yandex.ru>
2023-07-26 14:29:08 +0300

    Fix theme toggle issues

--- src/app/theme-toggle.tsx
+++ src/app/theme-toggle.tsx
@@ -1,24 +1,24 @@
 "use client";
 
 import { useTheme } from "next-themes";
-import {useEffect, useState} from "react";
+import {useEffect, useMemo, useState} from "react";
 import {SunIcon, MoonIcon} from "@heroicons/react/20/solid";
 
 export default function ThemeToggle() {
-    const { theme, setTheme } = useTheme();
+    const { resolvedTheme, setTheme } = useTheme();
     const [themeIcon, setThemeIcon] = useState(true);
 
     useEffect(() => {
-        setThemeIcon(theme === "dark");
-    }, [theme]);
+        setThemeIcon(resolvedTheme === "dark");
+    }, [resolvedTheme]);
 
-    const ThemeIcon = themeIcon ? SunIcon : MoonIcon;
+    const ThemeIcon = useMemo(() => themeIcon ? SunIcon : MoonIcon, [themeIcon]);
 
     return (
         <button
             className="rounded-md p-2 border-2 bg-neutral-100 border-neutral-200 text-neutral-900 focus:outline-none
             dark:bg-neutral-900 dark:border-neutral-800 dark:text-neutral-100 focus:ring ring-neutral-100 dark:ring-neutral-900"
-            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+            onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
         >
             <ThemeIcon className="w-4 h-4" />
         </button>
