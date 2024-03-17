141c43198a0e5b681fca2e13575f80a19d9c200a
Damir Modyarov <otomir@yandex.ru>
2024-03-17 20:09:10 +0300

    chore: Setup ESLint & Stylistic rules

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const stylistic = require("@stylistic/eslint-plugin")
+
+const customized = stylistic.configs.customize({
+    indent: 4,
+    quotes: "double",
+    semi: false,
+    jsx: true,
+})
+
+module.exports = {
+    extends: [
+        "next/core-web-vitals",
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    plugins: [
+        "@stylistic",
+        "@typescript-eslint",
+    ],
+    rules: {
+        ...customized.rules,
+        "@stylistic/jsx-one-expression-per-line": "off",
+        "@stylistic/max-len": ["warn", 120],
+        "@stylistic/no-trailing-spaces": ["error", { skipBlankLines: true }],
+        "@stylistic/member-delimiter-style": ["error", {
+            multiline: {
+                delimiter: "comma",
+                requireLast: true,
+            },
+            singleline: {
+                delimiter: "comma",
+                requireLast: false,
+            },
+        }],
+        "@stylistic/multiline-ternary": "off",
+        "@stylistic/brace-style": ["error", "1tbs", {
+            allowSingleLine: true,
+        }],
+        "@typescript-eslint/no-unused-vars": [
+            "error",
+            { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+        ],
+    },
+}
