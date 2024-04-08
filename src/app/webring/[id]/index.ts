f2f092403702b92d7838b3185436018a9e45d821
Damir Modyarov <otomir@yandex.ru>
2024-04-08 21:32:06 +0300

    fix: Make webring caching actually work
    sowwy next js caching is hawd >_<

--- src/app/webring/[id]/index.ts
+++ src/app/webring/[id]/index.ts
@@ -1,4 +1,4 @@
-import { getWebringSite } from "@/data/webring"
+import { getWebringSite, getWholeWebring } from "@/data/webring"
 
 export function getHandler(offset: number) {
     return async function GET(_: Request, { params: { id } }: { params: { id: string } }) {
@@ -6,4 +6,9 @@ export function getHandler(offset: number) {
         if (site === null) return new Response("Website is not part of my Webring", { status: 404 })
         return Response.redirect(site)
     }
+}
+
+export async function generateStaticParams() {
+    const sites = await getWholeWebring()
+    return sites.map(({ id }) => ({ id: String(id) }))
 }
