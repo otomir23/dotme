06c605503137142544ce0cec9394387a27d51f03
Damir Modyarov <otomir@yandex.ru>
2024-04-08 12:32:44 +0300

    feat: Add metadata & join link to the webring page

--- src/app/webring/page.tsx
+++ src/app/webring/page.tsx
@@ -3,6 +3,11 @@ import Link from "next/link"
 import { FlowerIcon } from "lucide-react"
 import ThemeToggle from "@/components/theme-toggle"
 
+export const metadata = {
+    title: "My Webring",
+    description: "This is a list of websites that are a part of my Webring. Contact me to join!",
+}
+
 export default async function Webring() {
     const sites = await getWholeWebring()
 
@@ -22,6 +27,7 @@ export default async function Webring() {
                         <span className="text-neutral-500"> – {site.url}</span>
                     </Link>
                 ))}
+                <Link href="/#socials" className="mt-4">Contact me to join {"-->"}</Link>
             </div>
         </>
     )
